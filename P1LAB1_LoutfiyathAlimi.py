ce686a19c10f17cb26e052fb09a96b8a2fee4f09
alimil3961 <alimil3961@student.faytechcc.edu>
2026-02-06 12:34:31 -0500

    Python Assignment

---
+++ P1LAB1_LoutfiyathAlimi.py
@@ -0,0 +1,33 @@
+# Loutfiyath Alimi
+# 2/6/2026
+# P1lab1
+# This program is to get imput from the user and display output
+
+# First and Last name
+
+your_first_name = "Jane"
+
+your_last_name = "Doe"
+
+
+
+# Display a message to the user
+print()
+print(f"enter your first name : {your_first_name}")
+print(f"enter your last name : {your_last_name}")
+print(f"Hello, {your_first_name} {your_last_name} ! Welcome to CTI-110 ")
+print()
+
+
+
+your_first_name = "LOUTFIYATH"
+
+your_last_name = "ALIMI"
+
+
+
+print()
+print(f"enter your first name : {your_first_name}")
+print(f"enter your last name : {your_last_name}")
+print(f"Hello, {your_first_name} {your_last_name} ! Welcome to CTI-110 ")
+print()
