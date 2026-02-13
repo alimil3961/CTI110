587af23f52d4eaaa9b60ca37cdaca9becbdd2aec
alimil3961 <alimil3961@student.faytechcc.edu>
2026-02-13 11:22:41 -0500

    Add files via upload

---
+++ P1HW1_AlimiLoutfiyath.py
@@ -0,0 +1,38 @@
+# Loutfiyath Alimi
+# 2/13/2026
+# P1HW1
+# This Project is to write a program that uses mathematical expressions
+
+print( "----------Calculating Exponents-----------")
+print("\n " )
+
+# Get 2 numbers from the user and raised to the power
+base_value = int(input("enter an interger: "))
+
+exponent_value = int(input("enter an enterger: "))
+
+# Get the result
+result = base_value ** exponent_value
+
+# Display the math to the user
+
+print("\n " )
+print(f"{base_value} raised to the power of {exponent_value} is {result}  !!")
+print()
+print()
+
+print( "-----------addition and Substraction-----------2")
+print("\n " )
+
+# Get 3 numbers from the user and add the first two and then substract
+num1 = int(input("enter 1st interger: "))
+num2 = int(input("enter an interger to add: "))
+num3 = int(input("enter an interger to substract: "))
+
+# Get the result
+result = num1 + num2 - num3
+
+# Display the math
+print("\n " )
+print(f"{num1} + {num2} - {num3} is equal to {result} ")
+print("\n " )
